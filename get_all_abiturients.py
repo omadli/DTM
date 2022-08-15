9030e296c4535aa4a48872bf841d356ac7b1324a
Omadli <omadliyigit17@gmail.com>
2022-08-15 20:27:10 +0500

    barcha abituriyentlarni olishga

--- get_all_abiturients.py
+++ get_all_abiturients.py
@@ -1,8 +1,23 @@
 import sys
 import asyncio
+import progressbar
 from db.main import Database
 from parser import DTM
 from asyncpg.exceptions import UniqueViolationError
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+
+progressbar.streams.wrap_stderr()
+def up():
+    sys.stdout.write('\x1b[1A')
+    sys.stdout.flush()
+
+def down():
+    sys.stdout.write('\n')
+    sys.stdout.flush()
 
 
 async def __main():
@@ -14,11 +29,15 @@ async def __main():
     
 SELECT s.id, u.RegionID, s.Uncode, u.name AS uname, s.facultyID, f.name AS fname, f.shifr, s.langID, s.mode  FROM Selections s 
 INNER JOIN Universities u ON s.Uncode=u.code INNER JOIN Faculties f ON s.facultyID=f.id 
-LEFT JOIN BoyevoySelections b ON b.selectionID=s.id WHERE b.selectionID IS NULL AND s.langID=1 ORDER BY u.regionID;    
+LEFT JOIN BoyevoySelections b ON b.selectionID=s.id WHERE b.selectionID IS NULL AND f.blokID=1 AND s.langID=1 ORDER BY u.regionID;    
     
     '''
     selections_list = await db.execute(sql1, fetch=True)
-    for sel in selections_list:
+    
+    down()
+    total = progressbar.ProgressBar(maxval=len(selections_list), redirect_stdout=True)
+    total.start()
+    for ssID, sel in enumerate(selections_list, 1):
         regionID = sel['regionid']
         unCode = sel['uncode']
         shifr = sel['shifr']
@@ -26,42 +45,59 @@ LEFT JOIN BoyevoySelections b ON b.selectionID=s.id WHERE b.selectionID IS NULL 
         ok = True
         p = 1
         n = 0
-        while ok:
-            try:
-                res = await dtm.Users_list(p, regionID, unCode, shifr, 1, 1)
-                if res is not None and res:
-                    # res['jami'] // 10 + 1  # max_page
-                    if n >= res['jami']:
+        res = await dtm.Users_list(p, regionID, unCode, shifr, 1, 1)
+        if res is not None and isinstance(res['jami'], int):
+            
+            sub = progressbar.ProgressBar(maxval=res['jami'], redirect_stdout=True)
+            sub.start()
+            up()
+            
+            while ok:
+                try:
+                    res = await dtm.Users_list(p, regionID, unCode, shifr, 1, 1)
+                    if res is not None and res:
+                        # res['jami'] // 10 + 1  # max_page
+                        if n >= res['jami']:
+                            ok = False
+                            break
+                        for abt in res['result']:
+                            ball = abt['ball']
+                            columns = ['abtID', 'name', 'langID']
+                            values = [int(abt['abtID']), abt['abtName'], 1]
+                            if ball is not None:
+                                columns.append('ball')
+                                values.append(ball)
+                            
+                            try:
+                                await db.insert_into('abiturients',columns, values)
+                                print(GREEN, f"{n+1})", abt['abtID'], abt['abtName'], ball, "bazaga qo'shildi", RESET)
+                            except UniqueViolationError as e:
+                                print(RED, f"{n+1})", abt['abtID'], abt['abtName'], ball, "bazada mavjud ekan", RESET)
+                            n += 1
+                        p += 1
+                    else:
                         ok = False
                         break
-                    for abt in res['result']:
-                        ball = abt['ball']
-                        columns = ['abtID', 'name', 'langID']
-                        values = [int(abt['abtID']), abt['abtName'], 1]
-                        if ball is not None:
-                            columns.append('ball')
-                            values.append(ball)
-                        
-                        try:
-                            await db.insert_into('abiturients',columns, values)
-                            print(f"{n+1})", abt['abtID'], abt['abtName'], ball, "bazaga qo'shildi")
-                        except UniqueViolationError as e:
-                            print(f"{n+1})", abt['abtID'], abt['abtName'], ball, "bazada mavjud ekan")
-                            
-                        n += 1
+                except Exception as e:
+                    print(e)
                     p += 1
-                else:
-                    ok = False
-                    break
-            except Exception as e:
-                print(e)
-                p += 1
+                    
+                sub.update(n)
+                
+            await db.execute('''INSERT INTO BoyevoySelections(selectionID, abts) VALUES ($1, $2);''', selectionID, n, execute=True)
+            
+            print(GREEN + f"\n\n\n{selectionID} {sel['uname']} {sel['fname']} bo'ldi\n\n\n" + RESET)
+        else:
+            print(RED + f"\n\n\n{selectionID} {sel['uname']} {sel['fname']} o'xshamayapti.\nAbiturientlar ro'yxati bo'sh shekilli\n\n\n" + RESET)
         
-        await db.execute('''UPDATE BoyevoySelections SET selectionID=$1, abts=$2''', selectionID, n, execute=True)
-        print(f"\n\n\n{selectionID} {sel['uname']} {sel['fname']} bo'ldi\n\n\n")
+        
+        sub.finish()
         dtm.change_user_agent()
+        total.update(ssID)
+        print()
             
     await dtm.close()
+    total.finish()
     print("Tugadi")
     
 if __name__ == '__main__':
